6fb167a3a16b674d315b791324298d8d7270d9b0
CJE90 <mrktup@hotmail.com>
2021-12-31 12:36:49 -0500

    refactoring file structure

--- index.js
+++ index.js
@@ -1,124 +1,135 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const Note = require('./models/note')
+const server = http.createServer(app);
 
-app.use(express.json())
-
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-}
-
-app.use(requestLogger);
-
-const cors = require('cors')
-
-app.use(cors())
-
-app.use(express.static('build'))
-
-
-app.get('/', (request, response) => {
-    console.log("firing from root")
-    response.send('<h1>Hello World!</h1>')
+server.listen(config.PORT, () => {
+    logger.info(`Server running on port ${config.PORT}`)
 })
 
-app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
-})
-
-app.get('/api/notes/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note) {
-                response.json(note)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
-})
-
-app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
-})
-
-app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
-
-    const note = {
-        content: body.content,
-        important: body.important,
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, { new: true })
-        .then(updatedNote => {
-            response.json(updatedNote)
-        })
-        .catch(error => next(error))
-})
-
-app.post('/api/notes', (request, response, next) => {
-    const body = request.body
-
-    const note = new Note({
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-    })
+// require('dotenv').config()
+// const express = require('express')
+// const app = express()
 
 
-    //Both below are correct, the first more explicit with promis chaining
-    note
-        .save()
-        .then(savedNote => savedNote.toJSON())
-        .then(savedAndFormattedNote => {
-            response.json(savedAndFormattedNote)
-        })
-        .catch(error => next(error))
+// const Note = require('./models/note')
 
-    // note.save().then(savedNote => {
-    //     response.json(savedNote.toJSON())
-    // })
-    //     .catch(error => next(error))
-})
+// app.use(express.json())
 
-const unknownEndpoint = (request, response) => {
-    console.log("an unknown endpoint is specified")
-    response.status(404).send({ error: 'unknown endpoint' })
-}
+// const requestLogger = (request, response, next) => {
+//     console.log('Method:', request.method)
+//     console.log('Path:  ', request.path)
+//     console.log('Body:  ', request.body)
+//     console.log('---')
+//     next()
+// }
 
-app.use(unknownEndpoint)
+// app.use(requestLogger);
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-    console.log("Endpoint should be reachable now")
-})
+// const cors = require('cors')
 
-const errorHandler = (error, request, response, next) => {
-    console.error(error.message)
+// app.use(cors())
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    }
-    else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
+// app.use(express.static('build'))
 
-    next(error)
-}
 
-app.use(errorHandler)
+// app.get('/', (request, response) => {
+//     console.log("firing from root")
+//     response.send('<h1>Hello World!</h1>')
+// })
+
+// app.get('/api/notes', (request, response) => {
+//     Note.find({}).then(notes => {
+//         response.json(notes)
+//     })
+// })
+
+// app.get('/api/notes/:id', (request, response, next) => {
+//     Note.findById(request.params.id)
+//         .then(note => {
+//             if (note) {
+//                 response.json(note)
+//             } else {
+//                 response.status(404).end()
+//             }
+//         })
+//         .catch(error => next(error))
+// })
+
+// app.delete('/api/notes/:id', (request, response, next) => {
+//     Note.findByIdAndRemove(request.params.id)
+//         .then(result => {
+//             response.status(204).end()
+//         })
+//         .catch(error => next(error))
+// })
+
+// app.put('/api/notes/:id', (request, response, next) => {
+//     const body = request.body
+
+//     const note = {
+//         content: body.content,
+//         important: body.important,
+//     }
+
+//     Note.findByIdAndUpdate(request.params.id, note, { new: true })
+//         .then(updatedNote => {
+//             response.json(updatedNote)
+//         })
+//         .catch(error => next(error))
+// })
+
+// app.post('/api/notes', (request, response, next) => {
+//     const body = request.body
+
+//     const note = new Note({
+//         content: body.content,
+//         important: body.important || false,
+//         date: new Date(),
+//     })
+
+
+//     //Both below are correct, the first more explicit with promis chaining
+//     note
+//         .save()
+//         .then(savedNote => savedNote.toJSON())
+//         .then(savedAndFormattedNote => {
+//             response.json(savedAndFormattedNote)
+//         })
+//         .catch(error => next(error))
+
+//     // note.save().then(savedNote => {
+//     //     response.json(savedNote.toJSON())
+//     // })
+//     //     .catch(error => next(error))
+// })
+
+// const unknownEndpoint = (request, response) => {
+//     console.log("an unknown endpoint is specified")
+//     response.status(404).send({ error: 'unknown endpoint' })
+// }
+
+// app.use(unknownEndpoint)
+
+// const PORT = process.env.PORT
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`)
+//     console.log("Endpoint should be reachable now")
+// })
+
+// const errorHandler = (error, request, response, next) => {
+//     console.error(error.message)
+
+//     if (error.name === 'CastError') {
+//         return response.status(400).send({ error: 'malformatted id' })
+//     }
+//     else if (error.name === 'ValidationError') {
+//         return response.status(400).json({ error: error.message })
+//     }
+
+//     next(error)
+// }
+
+// app.use(errorHandler)
